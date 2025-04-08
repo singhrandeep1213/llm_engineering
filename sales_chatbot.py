3a01ce7fa171841231cfb281c654b524bf4acd88
singhrandeep1213 <singh.randeep1213@gmail.com>
2025-04-08 12:13:56 +0530

    add pdf text

--- sales_chatbot.py
+++ sales_chatbot.py
@@ -1,22 +1,33 @@
 
 import os
 
-
 from dotenv import load_dotenv
 from openai import OpenAI, responses
-
+from PyPDF2 import PdfReader
 load_dotenv(override=True)
 openai_api_key=os.getenv("OPENAI_API_KEY")
 
 import gradio as gr
-
+file="test.pdf"
 if openai_api_key:
     print(f"open AI API key is present and begins with {openai_api_key[:8]}")
 
 openai=OpenAI()
 MODEL = 'gpt-4o-mini'
 
-system_message ="You are a helpful assistant in a clothes store. You should try to gently encourage the customer to try items that are on sale. Hats are 60% off, and most other items are 50% off. For example, if the customer says 'I'm looking to buy a hat', you could reply something like, 'Wonderful - we have lots of hats - including several that are part of our sales event. ' Encourage the customer to buy hats if they are unsure what to get."
+def extract_text_pdf(file):
+    if file is not None:
+        pdf_reader=PdfReader(file)
+        text=""
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+        return text
+
+
+pdf_data= extract_text_pdf(file)
+system_message ="You are a helful assistant that can process the below data in order to provide useful and interactive response:\n"
+system_message +=pdf_data
+# system_message ="You are a helpful assistant in a clothes store. You should try to gently encourage the customer to try items that are on sale. Hats are 60% off, and most other items are 50% off. For example, if the customer says 'I'm looking to buy a hat', you could reply something like, 'Wonderful - we have lots of hats - including several that are part of our sales event. ' Encourage the customer to buy hats if they are unsure what to get."
 
 def chat(message, history):
     # message=[
